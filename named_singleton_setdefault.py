070ce2bc0c286770624cf2812a34f88611b2634e
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-01 20:39:31 +0300

    Initial import
    Refactor Singleton.__call__ with setdefault

---
+++ named_singleton_setdefault.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        name = args[0]
+        saved = type(cls)._instances
+        if cls not in saved:
+            saved.setdefault(cls, {})
+        if name not in saved[cls]:
+            saved[cls][name] = super().__call__(*args, **kwargs)
+        return saved[cls][name]
+
+
+class Logger(metaclass=Singleton):
+    def __init__(self, name: str = "console"):
+        self.name = name
+        print(f"Initializing logger {name!r}")
+
+
+if __name__ == "__main__":
+    c1 = Logger("console")
+    c2 = Logger("console")
+    assert c1 is c2
+    s1 = Logger("stream")
+    s2 = Logger("stream")
+    assert s1 is s2
