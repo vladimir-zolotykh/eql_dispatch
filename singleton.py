f9c1c4ed17a49c79a9322de1aecd830411cbe772
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-01 18:50:11 +0300

    Refactor Singleton
    -    _instances = {}
    +    _instances = defaultdict(defaultdict)

--- singleton.py
+++ singleton.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from collections import defaultdict
+
+
 class Singleton(type):
-    _instances = {}
+    _instances = defaultdict(defaultdict)
 
     def __call__(cls, *args, **kwargs):
-        if cls not in (saved := type(cls)._instances):
-            saved[cls] = super().__call__(*args, **kwargs)
-        return saved[cls]
+        name = args[0]
+        if cls not in (saved := type(cls)._instances) or name not in saved[cls]:
+            saved[cls][name] = super().__call__(*args, **kwargs)
+        return saved[cls][name]
 
 
 class Logger(metaclass=Singleton):
-    def __init__(self):
-        print(f"Initializing {type(self)!r}")
+    def __init__(self, name: str = "console"):
+        self.name = name
+        print(f"Initializing logger {name!r}")
 
 
 if __name__ == "__main__":
-    g1 = Logger()
-    g2 = Logger()
-    assert g1 is g2
+    c1 = Logger("console")
+    c2 = Logger("console")
+    assert c1 is c2
+    s1 = Logger("stream")
+    s2 = Logger("stream")
+    assert s1 is s2
