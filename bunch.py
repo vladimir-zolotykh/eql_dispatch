9ba163648f9c84b50a12064f3b69f6da87a37989
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-02 17:46:05 +0300

    Fix Bunch.__new__
    -                raise ValueError(f"{list(kwargs)}: were not used")
    
    +                raise AttributeError(f"No slots left for {list(kwargs)}")
    
    -                del ns[key]
                     defaults[key] = val
    +                del ns[key]

--- bunch.py
+++ bunch.py
@@ -12,7 +12,8 @@ class BunchMeta(type):
                 val = kwargs.pop(key) if key in kwargs else defaults[key]
                 setattr(self, key, val)
             if kwargs:
-                raise ValueError(f"{list(kwargs)}: were not used")
+                # raise ValueError(f"{list(kwargs)}: were not used")
+                raise AttributeError(f"No slots left for {list(kwargs)}")
 
         def repr(self, *args, **kwargs):
             args = ", ".join(
@@ -29,12 +30,27 @@ class BunchMeta(type):
                 if key in reserved:
                     raise RuntimeError(f"Cannot overwrite {key}")
             else:
-                del ns[key]
                 defaults[key] = val
+                del ns[key]
         ns["__slots__"] = list(defaults)
         ns["__init__"] = init
         ns["__repr__"] = repr
         return super().__new__(mcls, name, bases, ns)
+
+
+class Test:
+    """
+    >>> bob = Person()
+    >>> bob
+    Person()
+    >>> max = Person(name='Max', age=42, salary=25000.0)
+    >>> max
+    Person(name='Max', age=42, salary=25000.0)
+    >>> bob = Person(name='Bob', age=43, job='Engineer')
+    Traceback (most recent call last):
+    ...
+    AttributeError: No slots left for ['job']
+    """
 
 
 class Person(metaclass=BunchMeta):
